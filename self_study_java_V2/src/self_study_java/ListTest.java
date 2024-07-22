a668665c086c7a3f28ddb13d08f83245564aa55b
WD <WD@WINDOWS-IH0D5O6>
2024-07-22 17:39:20 +0900

    fix : Member 클래스 추가

--- self_study_java_V2/src/self_study_java/ListTest.java
+++ self_study_java_V2/src/self_study_java/ListTest.java
@@ -3,7 +3,7 @@ package self_study_java;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListTest01 {
+public class ListTest {
 
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<String>();
---
+++ self_study_java_V2/src/self_study_java/Member.java
@@ -0,0 +1,21 @@
+package self_study_java;
+
+public class Member {
+	public String name;
+	public int age;
+	public Member(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(o instanceof Member) {
+			Member m = (Member) o;
+			return m.name.equals(name) && (m.age==age);
+		}else {
+			return false;
+		}
+	}
+}
